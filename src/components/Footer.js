2251486684087f4a2bfadf4963f2517ed1d58772
GalloisMax <max.gallois@outlook.fr>
2018-05-23 18:01:09 +0200

    Footer 2.0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,26 @@
 import React from "react"
 import { Container, Row, Col } from "reactstrap"
 
-export default class Footer extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>linkedin</Col>
-          <Col>projet</Col>
-          <Col>liens</Col>
-        </Row>
-        <Row>
-          <Col>linkedin</Col>
-          <Col>projet</Col>
-          <Col>liens</Col>
-        </Row>
-        <Row>
-          <Col>linkedin</Col>
-          <Col>projet</Col>
-          <Col>liens</Col>
-        </Row>
-      </Container>
-    )
-  }
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>linkedin</Col>
+        <Col>projet</Col>
+        <Col>liens</Col>
+      </Row>
+      <Row>
+        <Col>linkedin</Col>
+        <Col>projet</Col>
+        <Col>liens</Col>
+      </Row>
+      <Row>
+        <Col>linkedin</Col>
+        <Col>projet</Col>
+        <Col>liens</Col>
+      </Row>
+    </Container>
+  )
 }
+
+export default Footer
