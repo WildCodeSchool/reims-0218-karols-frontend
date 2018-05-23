3b0e6ac9c6604cf75489bf728adf4113961c2449
Cynnah <massot.dorian@gmail.com>
2018-05-23 10:59:39 +0200

    create a choiceshop component

--- src/components/ChoiceShop.js
+++ src/components/ChoiceShop.js
@@ -1,41 +1,39 @@
 import React from "react"
-import { Media } from "reactstrap"
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap"
 
-const ChoiceShop = () => {
-  return (
-    <Media>
-      <Media left href="#">
-        <Media
-          object
-          data-src="holder.js/64x64"
-          alt="Generic placeholder image"
-        />
-      </Media>
-      <Media body>
-        <Media heading>Media heading</Media>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-        fringilla. Donec lacinia congue felis in faucibus.
-        <Media>
-          <Media left href="#">
-            <Media
-              object
-              data-src="holder.js/64x64"
-              alt="Generic placeholder image"
-            />
-          </Media>
-          <Media body>
-            <Media heading>Nested media heading</Media>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-            scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum
-            in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-            nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-          </Media>
-        </Media>
-      </Media>
-    </Media>
-  )
+export default class ChoiceShop extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      dropdownOpen: false
+    }
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }))
+  }
+
+  render() {
+    return (
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>Change your shop</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    )
+  }
 }
-
-export default ChoiceShop
