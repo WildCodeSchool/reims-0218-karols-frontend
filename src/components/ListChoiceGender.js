c3e0d397cdb08dc3af86bb3639a6c0efe0389c8d
GalloisMax <max.gallois@outlook.fr>
2018-05-25 11:24:01 +0200

    fixed component ListChoiceGender

--- src/components/ListChoiceGender.js
+++ src/components/ListChoiceGender.js
@@ -1,11 +1,21 @@
 import React from "react"
-import ChoiceGender from "./ChoiceGender"
+import { FormGroup, Input, Label, Form } from "reactstrap"
 
-const ListChoiceGender = () => (
-  <div>
-    <ListChoiceGender />
-    <ListChoiceGender />
-  </div>
-)
+const ListChoiceGender = () => {
+  return (
+    <Form>
+      <FormGroup check>
+        <Label check>
+          <Input type="radio" name="gender" /> Femme
+        </Label>
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type="radio" name="gender" /> Homme
+        </Label>
+      </FormGroup>
+    </Form>
+  )
+}
 
 export default ListChoiceGender
