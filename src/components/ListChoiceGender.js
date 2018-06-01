95520d08c73be57d585b12af91bd90c34e5f8a08
Cynnah <massot.dorian@gmail.com>
2018-06-01 15:25:55 +0200

    creation of list choice gender

--- src/components/ListChoiceGender.js
+++ src/components/ListChoiceGender.js
@@ -1,19 +1,46 @@
 import React from "react"
-import { FormGroup, Input, Label } from "reactstrap"
+import { Card, CardTitle, Button, CardImg, CardBody } from "reactstrap"
 
-const ListChoiceGender = () => {
+const ListChoiceGender = ({ gender }) => {
   return (
     <div className="mt-4 mb-4">
-      <FormGroup check inline>
-        <Label check>
-          <Input type="radio" name="gender" /> Femme
-        </Label>
-      </FormGroup>
-      <FormGroup check inline>
-        <Label>
-          <Input type="radio" name="gender" /> Homme
-        </Label>
-      </FormGroup>
+      <div>
+        <Card
+          body
+          inverse
+          style={{ backgroundColor: "#333", borderColor: "#333" }}
+        >
+          <CardTitle style={{ fontSize: "2.5em" }}>
+            Choisissez votre sexe
+          </CardTitle>
+          <div className="d-flex">
+            <Card className=" m-3">
+              <CardImg
+                width="300"
+                height="300"
+                top
+                src="https://images.unsplash.com/photo-1448376561459-dbe8868fa34c?ixlib=rb-0.3.5ixid=eyJhcHBfaWQiOjEyMDd9s=361907f0deaaf183ac4ce6b62551dfb3auto=formatfit=cropw=1350q=80"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <Button>{gender}</Button>
+              </CardBody>
+            </Card>
+            <Card className="m-3">
+              <CardImg
+                top
+                width="300"
+                height="300"
+                src="https://images.unsplash.com/photo-1512236393941-3d6da97e56bc?ixlib=rb-0.3.5ixid=eyJhcHBfaWQiOjEyMDd9s=4deda26387ec695d02899757ecbad822auto=formatfit=cropw=1351q=80"
+                alt="Card image cap"
+              />
+              <CardBody className="text-center">
+                <Button>{gender}</Button>
+              </CardBody>
+            </Card>
+          </div>
+        </Card>
+      </div>
     </div>
   )
 }
