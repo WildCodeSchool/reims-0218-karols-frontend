8dbbdaaa3f3ae440317d2ac7d3606c44880c9c37
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-06-02 17:24:12 +0200

    calendar time change with style

--- src/components/CalendarTime.js
+++ src/components/CalendarTime.js
@@ -1,14 +1,27 @@
 import React from "react"
-import { Badge } from "reactstrap"
+import { Col } from "reactstrap"
 
 const CalendarTime = () => {
   return (
-    <div>
-      <a href="#" className="alert-link">
-        <p className="text-center">
-          <Badge color="secondary">09:00</Badge>
-        </p>
-      </a>
+    <div className="justify-content-center">
+      <div
+        style={{
+          position: "relative",
+          borderRadius: "4px",
+          width: "100%",
+          textAlign: "center",
+          margin: "10px 0",
+          height: "34px",
+          lineHeight: "34px",
+          fontSize: "14px",
+          fontWeight: "700",
+          cursor: "pointer",
+          backgroundColor: "#ccecfa"
+        }}
+      >
+        {" "}
+        13:30{" "}
+      </div>
     </div>
   )
 }
