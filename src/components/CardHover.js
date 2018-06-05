3d7fa5b0b93a0048bb74a6139cf6d45b65547077
GalloisMax <max.gallois@outlook.fr>
2018-06-05 11:02:08 +0200

    cardHover finish

--- src/components/CardHover.js
+++ src/components/CardHover.js
@@ -1,43 +1,66 @@
-import React from "react"
+import React, { Component } from "react"
 import {
   Card,
-  CardTitle,
-  CardText,
   CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
   CardImgOverlay,
-  CardFooter,
   Button
 } from "reactstrap"
 
-const CardHover = ({ name, description, image }) => {
-  return (
-    <div>
-      <Card inverse>
-        <CardImg width="100%" src={image} />
-        <CardImgOverlay-hover>
-          <CardTitle
-            style={{
-              fontSize: "2em"
-            }}
-          >
-            {name}
-          </CardTitle>
-          <CardText
-            style={{
-              fontSize: "1.5em"
-            }}
-          >
-            {description}
-          </CardText>
-        </CardImgOverlay-hover>
-        <CardFooter>
-          <Button color="default" size="lg" block>
-            Choisir
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
-  )
+class CardHover extends Component {
+  constructor(props) {
+    super(props)
+    this.handleMouseHover = this.handleMouseHover.bind(this)
+    this.state = {
+      isHovering: false
+    }
+  }
+  handleMouseHover() {
+    this.setState(this.toggleHoverState)
+  }
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div
+          onMouseEnter={this.handleMouseHover}
+          onMouseLeave={this.handleMouseHover}
+        >
+          <div>
+            <Card inverse>
+              <CardImg
+                width="100%"
+                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97270&w=318&h=270&bg=333333&txtclr=666666"
+                alt="Card image cap"
+              />
+              <CardImgOverlay>
+                {this.state.isHovering && <CardTitle> Card Title</CardTitle>}
+                {this.state.isHovering && (
+                  <CardText>Ca fonctionne plutôt bien</CardText>
+                )}
+                {this.state.isHovering && (
+                  <CardText>
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </CardText>
+                )}
+              </CardImgOverlay>
+            </Card>
+          </div>
+        </div>
+        {this.state.isHovering && <div />}
+      </div>
+    )
+  }
 }
 
 export default CardHover
+//{this.state.isHovering && (
