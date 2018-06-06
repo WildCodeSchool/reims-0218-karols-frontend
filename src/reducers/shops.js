ff92e6776daef4629446e5c67c627cd46859b819
Arno Wilder Reims <arnocrea@gmail.com>
2018-06-06 15:11:08 +0200

    modify shops reducer

--- src/reducers/shops.js
+++ src/reducers/shops.js
@@ -1,6 +1,22 @@
 import { CHOOSE_SHOP } from "../actions/actions"
 
-const initialState = []
+const initialState = [
+  {
+    id: 1,
+    name: "Paris",
+    selected: false
+  },
+  {
+    id: 2,
+    name: "Lyon",
+    selected: false
+  },
+  {
+    id: 3,
+    name: "Cannes",
+    selected: false
+  }
+]
 
 const shops = (prevState = initialState, action) => {
   if (action.type === CHOOSE_SHOP) {
