563755ec0d1b5360d237ec6ed2636f729f5be643
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-06-06 12:04:42 +0200

    add props in choice prestation img and name

--- src/components/ChoicePrestation.js
+++ src/components/ChoicePrestation.js
@@ -1,17 +1,13 @@
 import React from "react"
 import { Card, CardTitle, CardText, CardImgOverlay } from "reactstrap"
 
-const ChoicePrestation = () => {
+const ChoicePrestation = ({ imgSrc, name }) => {
   return (
     <div className="mb-4">
       <Card inverse>
-        <img
-          className="img-fluid"
-          src="https://images.unsplash.com/photo-1520173043194-dc6b2a237fee?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9da78404d02fafbd40afdf2b59dc2d9d&auto=format&fit=crop&w=800&q=60"
-          alt="Prestations"
-        />
+        <img className="img-fluid" src={imgSrc} alt={name} />
         <CardImgOverlay>
-          <CardTitle>Card Title</CardTitle>
+          <CardTitle> {name}</CardTitle>
           <CardText>Choisissez votre prestation</CardText>
         </CardImgOverlay>
       </Card>
