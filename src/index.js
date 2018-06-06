986c57c22a43898169c9d0288dd3963cbef16c20
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-06-06 12:05:13 +0200

    provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { createStore } from "redux"
+import reducers from "./reducers"
+import { Provider } from "react-redux"
 import "./index.css"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
 registerServiceWorker()
