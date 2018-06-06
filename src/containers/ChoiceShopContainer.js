1d2dca34f88b2144eb132e8a601ef65e3e49f152
Arno Wilder Reims <arnocrea@gmail.com>
2018-06-06 09:44:06 +0200

    add choice shop container

---
+++ src/containers/ChoiceShopContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import ChoiceShop from "../components/ChoiceShop"
+
+class ChoiceShopWrap extends Component {
+  render() {
+    return (
+      <div>
+        <ChoiceShop />
+      </div>
+    )
+  }
+}
+
+export default ChoiceShopWrap
