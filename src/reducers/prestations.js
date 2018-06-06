7e828ffca120ba40bb4e36af286104c78fc23023
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-06-06 12:05:33 +0200

    reducer prestation with action prestation_received

---
+++ src/reducers/prestations.js
@@ -0,0 +1,12 @@
+import { PRESTATIONS_RECEIVED } from "../actions/actions"
+
+const initialState = {}
+
+const prestations = (prevState = initialState, action) => {
+  if (action.type === PRESTATIONS_RECEIVED) {
+    return action.response
+  }
+  return prevState
+}
+
+export default prestations
