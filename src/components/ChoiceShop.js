6f9c334a32fc7260c6747426401a0fd37df4b635
Anahita <anahita.vahdani@neuf.fr>
2018-06-08 15:53:40 +0200

    modify choice chop component for a card

--- src/components/ChoiceShop.js
+++ src/components/ChoiceShop.js
@@ -1,61 +1,31 @@
 import React from "react"
-import {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from "reactstrap"
+import { Card, CardTitle, CardText, CardImgOverlay } from "reactstrap"
 
-export default class ChoiceShop extends React.Component {
-  constructor(props) {
-    super(props)
+const ChoiceShop = ({ city, image, adresse }) => {
+  return (
+    <div className="mb-4">
+      <Card inverse>
+        <img
+          className="img-fluid"
+          width="500"
+          height="250"
+          src={image}
+          alt="Name of service"
+        />
 
-    this.toggle = this.toggle.bind(this)
-    this.state = {
-      dropdownOpen: false
-    }
-  }
-
-  toggle() {
-    this.setState(prevState => ({
-      dropdownOpen: !prevState.dropdownOpen
-    }))
-  }
-
-  render() {
-    return (
-      <Dropdown
-        className="mt-3 mb-4"
-        isOpen={this.state.dropdownOpen}
-        toggle={this.toggle}
-      >
-        <DropdownToggle
-          caret
-          style={{
-            fontSize: "1.5em"
-          }}
-        >
-          Votre boutique
-        </DropdownToggle>
-        <DropdownMenu
-          className="text-center"
-          style={{
-            fontSize: "1.5em"
-          }}
-        >
-          <DropdownItem
+        <CardImgOverlay>
+          <CardTitle
             style={{
-              width: "200px"
+              fontSize: "2em"
             }}
           >
-            Paris
-          </DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Lyon</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Cannes</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    )
-  }
+            {city}
+          </CardTitle>
+          <CardText>{adresse}</CardText>
+        </CardImgOverlay>
+      </Card>
+    </div>
+  )
 }
+
+export default ChoiceShop
