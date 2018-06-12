fe94d3b93dead998a4e0219764499c431f862499
Anahita <anahita.vahdani@neuf.fr>
2018-06-12 15:51:25 +0200

    rename container

--- src/containers/PageContainer.js
+++ src/containers/PageContainer.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 
+import {
+  showServices,
+  showSex,
+  showFemalePrestation,
+  showMalePrestation
+} from "../display"
+
+import ServiceContainer from "./ServiceContainer"
+import ShopContainer from "./ShopContainer"
+import PrestationFemaleContainer from "./PrestationFemaleContainer"
+import PrestationMaleContainer from "./PrestationMaleContainer"
+import GenderContainer from "./GenderContainer"
+
 import { fetchShopsPrestations } from "../api"
 
 class Page extends Component {
