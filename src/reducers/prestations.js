3eefedce51c21d24820bf110afe67c317fc6a770
Cynnah <massot.dorian@gmail.com>
2018-06-13 11:36:34 +0200

    conditional rendering en men prestation

--- src/reducers/prestations.js
+++ src/reducers/prestations.js
@@ -7,9 +7,19 @@ const initialState = []
 
 const prestations = (prevState = initialState, action) => {
   if (action.type === CHOOSE_PRESTATION) {
+    // si action type = choose_prestation alors =>
     return prevState.map(prestation => ({
-      ...prestation,
-      selected: action.prestationId === prestation.id
+      //return prevstate parcours chaque prestation
+      ...prestation, // destructuration de l'objet
+      preparations: prestation.preparations.map(preparation => ({
+        ...preparation,
+        selected:
+          action.prestationId === prestation.id
+            ? preparation.id === action.preparationId
+              ? !preparation.selected
+              : false
+            : preparation.selected
+      }))
     }))
   }
   if (action.type === SHOPS_PRESTATIONS_RECEIVED) {
