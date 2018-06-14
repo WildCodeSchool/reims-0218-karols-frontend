8ac221c841cf966d921007071426f6d06fd0c2f1
Arno Wilder Reims <arnocrea@gmail.com>
2018-06-14 12:10:53 +0200

    add datepickercontainer

---
+++ src/containers/DatePickerContainer.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react"
+import InfiniteCalendar from "react-infinite-calendar"
+import "react-infinite-calendar/styles.css" // Make sure to import the default stylesheet
+
+// Render the Calendar
+var today = new Date()
+var lastWeek = new Date(
+  today.getFullYear(),
+  today.getMonth(),
+  today.getDate() - 7
+)
+
+class DatePickerSelect extends Component {
+  state = {
+    date: new Date()
+  }
+
+  onChange = date => this.setState({ date })
+
+  render() {
+    return (
+      <div className="justify-content-center">
+        <InfiniteCalendar
+          style={{ margin: "auto" }}
+          width={400}
+          height={400}
+          selected={today}
+          disabledDays={[0, 7]}
+          minDate={lastWeek}
+          displayOptions={{
+            showHeader: true,
+            overscanMonthCount: 4,
+            showWeekdays: true,
+            shouldHeaderAnimate: true
+          }}
+          locale={{
+            weekStartsOn: 1,
+            locale: require("date-fns/locale/fr"),
+            headerFormat: "dddd, D MMM",
+            weekdays: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+            blank: "Aucune date sélectionnée",
+            todayLabel: {
+              long: "Aujourd'hui",
+              short: "Auj."
+            }
+          }}
+          theme={{
+            selectionColor: "rgb(146, 118, 255)",
+            textColor: {
+              default: "#333",
+              active: "#FFF"
+            },
+            weekdayColor: "rgb(146, 118, 255)",
+            headerColor: "rgb(127, 95, 251)",
+            floatingNav: {
+              background: "rgba(81, 67, 138, 0.96)",
+              color: "#FFF",
+              chevron: "#FFA726"
+            }
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+export default DatePickerSelect
