3e47ad77fc11439bf919945709c9112e557e108d
Anahita <anahita.vahdani@neuf.fr>
2018-06-20 16:05:17 +0200

    merge max branch : change props name

--- src/containers/ResumeContainer.js
+++ src/containers/ResumeContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
+import { Jumbotron } from "reactstrap"
 
 import {
   getSelectedShop,
@@ -18,17 +19,41 @@ const mapStateToProps = state => ({
 class ShowResume extends Component {
   render() {
     return (
-      <div>
-        {this.props.selectedShop && <p>{this.props.selectedShop.city}</p>}
-        {this.props.selectedService && <p>{this.props.selectedService.name}</p>}
-        {this.props.selectedGender && <p>{this.props.selectedGender.sex}</p>}
+      <Jumbotron
+        style={{
+          textAlign: "center",
+          backgroundColor: "#d7dbe2",
+          fontSize: "25px",
+          borderRadius: "100px"
+        }}
+      >
+        <h1 className="display-12">Récapitulatif</h1>
+        {this.props.selectedShop && (
+          <p className="shop">
+            Votre réservation se fera à {this.props.selectedShop.city}
+          </p>
+        )}
+        <hr className="my-2" />
+        {this.props.selectedService && (
+          <p className="préparation">
+            Vous avez choisi {this.props.selectedService.name}
+          </p>
+        )}
+        {this.props.selectedGender && (
+          <p className="gender">Pour {this.props.selectedGender.sex}</p>
+        )}
         {this.props.selectedPreparations &&
           this.props.selectedPreparations.map((preparation, index) => {
             return (
-              <p key={index}>{preparation.preparations[0].titlePreparation}</p>
+              <p className="prestation" key={index}>
+                {preparation.preparations[0].titlePreparation}
+              </p>
             )
+            // <p className="horaire">
+            //   Vous désirez être pris en charge le {horaire}{" "}
+            // </p>
           })}
-      </div>
+      </Jumbotron>
     )
   }
 }
