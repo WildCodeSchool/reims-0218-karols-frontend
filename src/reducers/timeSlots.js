4ed557091a719e873ed25868aca6b3a68f73ee03
Cynnah <massot.dorian@gmail.com>
2018-06-21 18:08:30 +0200

    test ok

--- src/reducers/timeSlots.js
+++ src/reducers/timeSlots.js
@@ -6,17 +6,17 @@ const initialState = []
 const timeSlot = (prevState = initialState, action) => {
   if (action.type === CHOOSE_TIMESLOT) {
     // si l'action est de type CHOOSE_TIMESLOT
-    return prevState.map(timeSlot =>
-      timeSlot.timeSlots.map(timeSlot => ({
+    return prevState.map(day => ({
+      ...day,
+      timeSlots: day.timeSlots.map(timeSlot => ({
         ...timeSlot,
         selected: action.timeSlot.time.s === timeSlot.time.s
       }))
-    )
+    }))
   }
   if (action.type === TIMESLOTS_RECEIVED) {
     return action.response
   }
   return prevState
 }
-
 export default timeSlot
