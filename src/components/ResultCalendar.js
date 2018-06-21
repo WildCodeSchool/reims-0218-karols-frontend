22e2b865674acbb21138005e575b0236401c6c20
Tanguy Marlot <marlot.tanguy@orange.fr>
2018-06-21 22:57:13 +0200

    SelectTimeSlot in ResultCalendar

--- src/components/ResultCalendar.js
+++ src/components/ResultCalendar.js
@@ -1,11 +1,15 @@
 import React from "react"
 import ListCalendarDayTime from "./ListCalendarDayTime"
 
-const ResultCalendar = ({ weekTimeSlots }) => {
+const ResultCalendar = ({ weekTimeSlots, selectTimeSlot }) => {
   return (
     <div className="d-flex justify-content-center">
       {weekTimeSlots.map((dayTimeSlot, index) => (
-        <ListCalendarDayTime key={index} day={dayTimeSlot} />
+        <ListCalendarDayTime
+          key={index}
+          day={dayTimeSlot}
+          selectTimeSlot={selectTimeSlot}
+        />
       ))}
     </div>
   )
