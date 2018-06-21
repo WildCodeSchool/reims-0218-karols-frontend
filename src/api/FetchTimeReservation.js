de582cb567e883411aa98469579b1cb978b0b84f
Cynnah <massot.dorian@gmail.com>
2018-06-21 23:34:08 +0200

    first commit

---
+++ src/api/FetchTimeReservation.js
@@ -0,0 +1,10 @@
+//     fetch("https://localhost:8000/reservations", {
+//       method: "POST",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({ selectedShop: getSelectedShop(state) })
+//     })
+//   }
+// }
