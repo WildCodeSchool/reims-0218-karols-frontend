8111a30d9ac9626e700c5a7d0cc5346aa0de8bfa
GalloisMax <max.gallois@outlook.fr>
2018-06-27 14:15:39 +0200

    delete iFrame

--- src/components/iFrame.js
+++
@@ -1,9 +0,0 @@
-// import Iframe from 'react-iframe'
-// <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
-//         width="450px"
-//         height="450px"
-//         id="myId"
-//         className="myClassname"
-//         display="initial"
-//         position="relative"
-//         allowFullScreen/>
