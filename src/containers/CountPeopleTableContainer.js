c80b509c4267b1abc29ae33f9309da28be1d0734
GalloisMax <max.gallois@outlook.fr>
2018-07-03 14:29:39 +0200

    display card table + count table

--- src/containers/CountPeopleTableContainer.js
+++ src/containers/CountPeopleTableContainer.js
@@ -1,49 +1,11 @@
-// import React, { Component } from "react"
-// import { connect } from "react-redux"
-
-// import { makeChooseCountPeopleTable } from "../actions/actions"
-
-// import SelectClient from "../components/SelectClient"
-
-// const mapStateToProps = state => ({
-//   count: state.count
-// })
-
-// const mapDispatchToProps = dispatch => ({
-//   onCountChange: count => dispatch(makeChooseCountPeopleTable(count))
-// })
-
-// class CountPeopleTable extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <SelectClient
-//           onCountChange={event => {
-//             this.props.onCountChange(event.target.value)
-//           }}
-//           count={this.props.count}
-//         />
-//       </div>
-//     )
-//   }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(CountPeopleTable)
-
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { Row, Col } from "reactstrap"
 import { scroller } from "react-scroll"
 import Zoom from "react-reveal/Zoom"
 
-import {
-  // makeChoosecount,
-  makeIncrementTable,
-  makeDecrementTable
-} from "../actions/actions"
-// import GenderTitle from "../components/GenderTitle"
-// import CardModelGender from "../components/CardModelGender"
-// import SelectClient from "../components/SelectClient"
+import { makeIncrementTable, makeDecrementTable } from "../actions/actions"
+
 import CardModelTable from "../components/CardModelTable"
 import TableTitle from "../components/TableTitle"
 import { showCounter } from "../display"
@@ -59,7 +21,6 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  //select: count => dispatch(makeChoosecount(count)),
   handlePlus: count => dispatch(makeIncrementTable(count)),
   handleMinus: count => dispatch(makeDecrementTable(count))
 })
@@ -90,12 +51,6 @@ class Table extends Component {
                 select={true}
                 showCounter={this.props.showCounter}
               />
-              {/* <CardModelGender
-                  id={gender.count}
-                  select={this.props.select}
-                  {...gender}
-                  showCounter={this.props.showCounter}
-                /> */}
             </Zoom>
           </Col>
         </Row>
--- src/reducers/countPeopleTable.js
+++ src/reducers/countPeopleTable.js
@@ -1,21 +1,5 @@
-// import { CHOOSE_COUNT_PEOPLE } from "../actions/actions"
 import { INCREMENT_TABLE } from "../actions/actions"
 import { DECREMENT_TABLE } from "../actions/actions"
-
-// const initialState = {
-//   count: 2
-// }
-// const countPeopleTable = (prevState = initialState, action) => {
-//   if (action.type === CHOOSE_COUNT_PEOPLE) {
-//     return {
-//       ...prevState,
-//       count: action.count
-//     }
-//   }
-//   return prevState
-// }
-
-// export default countPeopleTable
 
 const initialState = {
   count: 2,
@@ -23,12 +7,6 @@ const initialState = {
 }
 
 const countPeopleTable = (prevState = initialState, action) => {
-  // if (action.type === CHOOSE_SEX) {
-  //   return prevState.map(sexObject => ({
-  //     ...sexObject,
-  //     selected: action.sex === sexObject.sex
-  //   }))
-  // }
   if (action.type === INCREMENT_TABLE) {
     return {
       ...prevState,
