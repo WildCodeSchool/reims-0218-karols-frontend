872feda758a67980dc4961583fe3fb0dae97e122
Anahita <anahita.vahdani@neuf.fr>
2018-07-03 22:39:19 +0200

    add new logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,7 @@
 import React from "react"
 
 const Logo = () => (
-  <img
-    src="http://res.cloudinary.com/dlfnke6kc/image/upload/v1527837786/karols-600x600px_fdxlcu.jpg"
-    className="w-25 p-3"
-    alt="logo"
-  />
+  <img src="../images/logo.png" className="w-25 p-3" alt="logo" />
 )
 
 export default Logo
