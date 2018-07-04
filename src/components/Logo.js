3aa1530e30b9129a0e5f2086d075b8012ba68634
Anahita <anahita.vahdani@neuf.fr>
2018-07-04 09:41:41 +0200

    change logo url

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Logo = () => (
-  <img src="../images/logo.png" className="w-25 p-3" alt="logo" />
+  <img src="https://image.noelshack.com/fichiers/2018/27/3/1530690032-logo-noirt.png" className="w-25 p-3" alt="logo" />
 )
 
 export default Logo
