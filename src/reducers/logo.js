f20546c317339f21fb49d01d182a317e51330907
Cynnah <massot.dorian@gmail.com>
2018-07-05 17:33:28 +0200

    create logo reducer

---
+++ src/reducers/logo.js
@@ -0,0 +1,9 @@
+import { SHOPS_PRESTATIONS_RECEIVED } from "../actions/actions"
+
+const initialState = []
+
+const Logo = () => (
+  <img src="https://image.noelshack.com/fichiers/2018/27/3/1530690032-logo-noirt.png" className="w-25 p-3" alt="logo" />
+)
+
+export default logo
