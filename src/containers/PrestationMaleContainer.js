13139b30153d38d3d653e0deee6f2c3611021ab4
Anahita <anahita.vahdani@neuf.fr>
2018-07-17 17:36:43 +0200

    add max count on reducer and container

--- src/containers/PrestationMaleContainer.js
+++ src/containers/PrestationMaleContainer.js
@@ -10,12 +10,14 @@ import { scroller } from "react-scroll"
 import Zoom from "react-reveal/Zoom"
 
 import { showCounter } from "../display"
+import { getCountByGender } from "../resume/index"
 
 const mapStateToProps = state => ({
   prestations: state.prestations.filter(
     prestation => prestation.gender === "M"
   ),
-  showCounter: showCounter(state)
+  showCounter: showCounter(state),
+  maxCountMale: getCountByGender(state, "M")
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -24,8 +26,8 @@ const mapDispatchToProps = dispatch => ({
   handleMinus: (prestationId, preparationId) => {
     dispatch(makeDecrementPrestation(prestationId, preparationId))
   },
-  handlePlus: (prestationId, preparationId) => {
-    dispatch(makeIncrementPrestation(prestationId, preparationId))
+  handlePlus: max => (prestationId, preparationId) => {
+    dispatch(makeIncrementPrestation(prestationId, preparationId, max))
   }
 })
 
@@ -45,7 +47,7 @@ class MaleSelected extends Component {
             {...this.props}
             showCounter={this.props.showCounter}
             handleMinus={this.props.handleMinus}
-            handlePlus={this.props.handlePlus}
+            handlePlus={this.props.handlePlus(this.props.maxCountMale)}
           />
         </Zoom>
       </div>
